925bc0866caa60e0b6e9ac86dc26da3d7ac92ce7
Oleg <oleg1248163264@gmail.com>
2020-03-15 03:12:47 +0300

    api for model Author, only GET requests work

--- exlibris/serializers.py
+++ exlibris/serializers.py
@@ -1,13 +1,8 @@
 from rest_framework import serializers
 from .models import Author, Biography, Book, Reader, Lend
 
-class AuthorSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
 
-    def create(self, validated_data):
-        return(Author.objects.create(**validated_data))
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        return instance
-
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name']
--- exlibris/urls.py
+++ exlibris/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('api/v1/', ),
-    #path('api/vi/<int:id>',  )
+    path('api/v1', views.author_list),
+    path('api/v1/<int:pk>', views.snippet_detail)
 ]
